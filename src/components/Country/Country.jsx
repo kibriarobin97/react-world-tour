887856ba5e2c0c1905b0d832bd3c1d1c9480483b
kibriarobin97 <kibriarobin97@gmail.com>
2024-03-10 22:48:42 +0600

    added country

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,13 @@
+import './Country.css'
+
+const Country = ({country}) => {
+    const {name, flags} = country;
+    return (
+        <div className='country'>
+            <img src={flags?.png} alt="" />
+            <h3>Name: {name?.common}</h3>
+        </div>
+    );
+};
+
+export default Country;
